4672bebabd34e87645b6553ef38f9fcc13a28b79
Bryan Rodrigues <bryan.rodrigues@wansoft.com.br>
2024-04-29 10:24:36 -0300

    Finalização da tabela  modal

---
+++ src/components/Modal/styles.js
@@ -0,0 +1,139 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin: 50px 0;
+    width: 100%;
+    height: 600px;
+    color: #6F6F6F;
+    .linha{
+        display: flex;
+        flex-flow: row wrap;
+    }
+    .margin-top-5{
+        margin-top: 5px;
+
+    }
+    .coluna-1 {
+        width: calc((100% ) * (1 / 12)); 
+    }
+    
+    .coluna-2 {
+        width: calc(((100%) * (2 / 12)) - 10px); 
+        margin: 0px 5px;
+        display: flex;
+    }
+    
+    .coluna-3 {
+        width: calc((100% ) * (3 / 12)); 
+    }
+    
+    .coluna-4 {
+        width: calc(((100%) * (4 / 12)) - 10px); 
+        margin: 0px 5px;
+    }
+    
+    .coluna-5 {
+        width: calc((100% ) * (5 / 12)); 
+    }
+    
+    .coluna-6 {
+        width: calc( ( (100%) * (6 / 12) ) - 5px );
+        margin: 0 2.5px; 
+    }
+    
+    .coluna-7 {
+        width: calc((100% ) * (7 / 12)); 
+    }
+    
+    .coluna-8 {
+        width: calc((100% ) * (8/ 12)); 
+        
+    }
+    
+    .coluna-9 {
+        width: calc((100% ) * (9 / 12)); 
+    }
+    
+    .coluna-10 {
+        width: calc((100% ) * (10 / 12));
+    }
+    
+    .coluna-11 {
+        width: calc((100% ) * (11 / 12));
+    }
+    
+    .coluna-12 {
+        width: calc(100% )
+    }
+    .flex{
+        display: flex;
+    }
+    .center{
+        justify-content: center;
+    }
+    .obs{
+        font-size:12px;
+        text-align: left;
+        padding-left: 10px;
+        margin: 0
+    }
+    .title{
+        // font-family: Montserrat;
+        font-size: 28px;
+        font-weight: 500;
+        line-height: 0px;
+        text-align: left;        
+    }
+    .text{
+        // font-family: Montserrat;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 0px;
+        text-align: left;
+    }
+    .gray{
+        display: flex;
+        color: white;
+        background: #B6B6B6;
+        border-radius: 20px;
+        opacity: 0.4px;
+        height: 36px;
+        font-size: 14px;
+        justify-content: center;
+        align-items: center;
+    }
+    .white{
+        display: flex;
+        background: white;
+        border-radius: 20px;
+        height: 36px;
+        font-size: 14px;
+        justify-content: left;
+        align-items: center;
+    }
+    .padding-10{
+        padding-left: 20px
+    }
+    .dark-blue{
+        display: flex;
+        color: white;
+        background: #7367F0        ;
+        border-radius: 20px;
+        opacity: 0.4px;
+        height: 36px;
+        font-size: 14px;
+        justify-content: center;
+        align-items: center;
+    }
+    .light-blue{
+        display: flex;
+        color: white;
+        background: #43CBFF;
+        border-radius: 20px;
+        opacity: 0.4px;
+        height: 36px;
+        font-size: 14px;
+        justify-content: center;
+        align-items: center;
+    }
+`;
--- src/components/Section2/index.jsx
+++ src/components/Section2/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Icone } from "./styles";
 import about from "../../assets/Section2/About.png"
+import Modal from "../Modal"; 
+
 const rows = [
     {
         days: `NA HORA`,
@@ -17,72 +19,87 @@ const rows = [
 ];
 
 export default function Section2() {
+    const [modalOpen, setModalOpen] = useState(false);
 
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
     return (
-        <Container>
-            <div className='container'>
-                <div className="slide">
-                    <div className='column1'>
-                        <div className='center'>
-                            <div className='title'>
-                                Simulador de vendas mpays
-                            </div>
-                            <br />
-                            <div className='text'>
-                                Veja quanto você recebe nas vendas no crédito, débito e parcelado.
+        <>
+            <Container>
+                <div className='container'>
+                    <div className="slide">
+                        <div className='column1'>
+                            <div className='center'>
+                                <div className='title'>
+                                    Simulador de vendas mpays
+                                </div>
+                                <br />
+                                <div className='text'>
+                                    Veja quanto você recebe nas vendas no crédito, débito e parcelado.
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='column2'>
-                        <div className="insert">
-                            <form name="calcularValor" action="#" method="post">
-                                <label htmlFor="calcularValor">
-                                    Valor
-                                </label>
-                                <br />
-                                <input type="text" name="value" disabled placeholder={'R$ 1.000,00'}/>
-                                <br />
-                                <br />
-                                <label htmlFor="calcularValor">
-                                    Forma de pagamento
-                                </label>
-                                <br />
-                                <select name="payment" id="payment" disabled value={0}>
-                                    <option value="pix">PIX</option>
-                                    <option value="0">Crédito à vista</option>
-                                </select>
-                                <br />
-                            </form>
-                        </div>
-                        <div  className="values"> 
-                            <table>
-                                <tr>
-                                    <th colspan="2">
-                                        VOCÊ RECEBE:
-                                    </th>
-                                </tr>
-                                {
-                                    rows.map((row) => (
-                                        <tr>
-                                            <td>
-                                                {row.days}
-                                            </td>
-                                            <td>
-                                                {row.value}
-                                            </td>
-                                        </tr>
-                                    ))
-                                }
-                            </table>
-                            <p>
-                                Todas as simulações usam as taxas iniciais informadas no site.
-                                <Icone src={about} alt="Sobre" />
-                            </p>
+                        <div className='column2'>
+                            <div className="insert">
+                                <form name="calcularValor" action="#" method="post">
+                                    <label htmlFor="calcularValor">
+                                        Valor
+                                    </label>
+                                    <br />
+                                    <input type="text" name="value" disabled placeholder={'R$ 1.000,00'} />
+                                    <br />
+                                    <br />
+                                    <label htmlFor="calcularValor">
+                                        Forma de pagamento
+                                    </label>
+                                    <br />
+                                    <select name="payment" id="payment" disabled value={0}>
+                                        <option value="pix">PIX</option>
+                                        <option value="0">Crédito à vista</option>
+                                    </select>
+                                    <br />
+                                </form>
+                            </div>
+                            <div className="values">
+                                <table>
+                                    <tr>
+                                        <th colspan="2">
+                                            VOCÊ RECEBE:
+                                        </th>
+                                    </tr>
+                                    {
+                                        rows.map((row) => (
+                                            <tr>
+                                                <td>
+                                                    {row.days}
+                                                </td>
+                                                <td>
+                                                    {row.value}
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </table>
+                                <p>
+                                    Todas as simulações usam as taxas iniciais informadas no site.
+                                    <Icone src={about} alt="Sobre"  onClick={openModal}/>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </Container>
+            <div>
+                <Modal isOpen={modalOpen} onClose={closeModal} /> 
             </div>
-        </Container>
+        </>
+
+
     )
 }
